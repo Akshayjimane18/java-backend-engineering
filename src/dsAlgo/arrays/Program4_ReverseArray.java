97105cc0b8a5f9bc24a038211ed3aabbb36fbaf2
akshayjimane18 <akshayjimane18@gmail.com>
2026-03-01 15:24:58 +0530

    Resolved Array: Reverse Array

---
+++ src/dsAlgo/arrays/Program4_ReverseArray.java
@@ -0,0 +1,28 @@
+package dsAlgo.arrays;
+
+import java.util.Arrays;
+
+public class Program4_ReverseArray {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        int [] a = {2, 423, 23, 2, 534, 22, 754,2};
+
+        System.out.println("Reversed array = "+ Arrays.toString(reversedArray(a)));
+
+    }
+
+    static int[] reversedArray(int[] arr){
+        int temp;
+        int n = arr.length-1;
+
+        for(int i=0;i<arr.length;i++){
+            temp = arr[i];
+            arr[i] = arr[n];
+            arr[n] = temp;
+            n--;
+        }
+
+        return arr;
+    }
+}
