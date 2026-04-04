8a6979c92704941e8680a733013d149d90c87bae
akshayjimane18 <akshayjimane18@gmail.com>
2026-04-04 20:38:18 +0530

    Striver - Completed whole number, character pattern programs

---
+++ src/dsAlgo/striverA2Z/printPatterns/Pattern_18.java
@@ -0,0 +1,27 @@
+package dsAlgo.striverA2Z.printPatterns;
+
+public class Pattern_18 {
+
+    /**
+     * E
+     * D E
+     * C D E
+     * B C D E
+     * A B C D E
+     */
+    public static void main(String[] args) {
+        int n = 5;
+        Pattern_18 obj = new Pattern_18();
+        obj.pattern18(n);
+    }
+
+    public void pattern18(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i - 1; j <= n - 1; j++) {
+                System.out.print((char) ('A' + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/dsAlgo/striverA2Z/printPatterns/Pattern_21.java
@@ -0,0 +1,34 @@
+package dsAlgo.striverA2Z.printPatterns;
+
+public class Pattern_21 {
+
+    /**
+     * *****
+     * *   *
+     * *   *
+     * *   *
+     * *****
+     */
+    public static void main(String[] args) {
+        int n = 5;
+        Pattern_21 obj = new Pattern_21();
+        obj.pattern21(n);
+    }
+
+    public void pattern21(int n) {
+        for (int j = 0; j < n; j++) {
+
+            for (int i = 0; i < n; i++) {
+
+                if (j == 0 || j == n - 1 || i == 0 || i == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+
+            System.out.println();
+        }
+    }
+}
